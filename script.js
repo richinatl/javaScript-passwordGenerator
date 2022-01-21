0097a0f2eb09bdd385d67313566b863653368a2b
Richard Murphy <richinatl@gmail.com>
2022-01-20 23:15:43 -0500

    updated README, completed javaScript

---
+++ script.js
@@ -0,0 +1,74 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// First gather required data
+
+let length = Number(prompt("How many characters would you like your password to be?"));
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+
+let upperCase = confirm("Would you like to use uppercase letters?");
+let lowerCase = confirm("Would you like to use lowercase letters?");
+let numbers = confirm("Would you like to use numbers?");
+let symbols = confirm("Would you like to symbols?");
+
+while (!upperCase && !lowerCase && !numbers && !symbols) {
+  alert("You must select at least one character type!");
+  upperCase = confirm("Would you like to use uppercase letters?");
+  lowerCase = confirm("Would you like to use lowercase letters?");
+  numbers = confirm("Would you like to use numbers?");
+  symbols = confirm("Would you like to use symbols?");
+}
+
+// The generate password function took three steps
+
+// First created a function to make arrays
+
+function charFromLowtoHigh(low,high) {
+  let array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array
+}
+
+// Second, use the array function I created to make arrays from the ascii codes
+
+const lowerChar = charFromLowtoHigh(97,122);
+const upperChar = charFromLowtoHigh(65,90);
+const numberChar = charFromLowtoHigh(48,57);
+const symbolChar = charFromLowtoHigh(34,47);
+
+
+// Third, the generate password function itself
+function generatePassword (UpperCase, Numbers, Symbols) {
+  // I set the lowercase as default, then added if statements for the others
+  let charCodes = lowerChar
+  if (UpperCase = true) charCodes = charCodes.concat(upperChar)
+  if (Numbers = true) charCodes = charCodes.concat(numberChar)
+  if (Symbols = true) charCodes = charCodes.concat(symbolChar)
+  
+  let Password = []
+
+// Made a for loop, then pushed to characters into the empty array
+// Converted the array to a string to complete the project
+  for (let i = 0; i < length; i++) {
+    let character = charCodes[Math.floor(Math.random() * charCodes.length)]
+    Password.push(String.fromCharCode(character));
+    
+  }
+  return Password.join('');
+}
